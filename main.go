8e2faca8dbba4d59ce787315effcf20db05852eb
Adeem <75906616+adeemgoogle@users.noreply.github.com>
2023-11-14 09:40:05 +0600

    Update main.go

--- main.go
+++ main.go
@@ -1,26 +1,14 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/log"
-	"github.com/joho/godotenv"
-	"tz/internal/routes"
-	"tz/pkg/database"
+	"log"
+	"tz/internal/server"
 )
 
 func main() {
-	app := fiber.New()
 
-	err := godotenv.Load()
-	db := database.InitDB()
-
-	if db == nil {
-		log.Fatal("Failed to initialize the database")
-	}
-
-	routes.Requests(app, db)
-	err = app.Listen(":8080")
+	err := server.Start()
 	if err != nil {
-		return
+		log.Fatal("App couldnt start")
 	}
 }
